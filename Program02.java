df4f354c95e91c34f9cd50fff87ddd413eca1aa7
parthpatel2274 <parthpatel7035@gmail.com>
2024-03-27 16:15:30 +0530

    1 to 5

---
+++ Program02.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Program02 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter 1st No. :-");
+            int a = sc.nextInt();
+            System.out.println("Enter 2nd No. :-");
+            int b = sc.nextInt();
+            int sum = a + b;
+            int diff = a - b;
+            int prod = a * b;
+            int qoutient = a / b;
+            System.out
+                    .println(
+                            "Sum = " + sum + "\nDifference = " + diff + "\nProduct = " + prod + "\nQoutient = "
+                                    + qoutient);
+        }
+
+    }
+}
---
+++ Program05.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Program05 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a Number :-");
+            int a = sc.nextInt();
+            if (a % 2 == 0) {
+                System.out.println(a + " is Even.");
+            } else {
+                System.out.println(a + " is Odd.");
+            }
+        }
+    }
+}
+
